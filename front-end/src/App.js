157b78ef35af16c528ed3afe6a0f50f4c018bb07
kitan <oademid2@uwo.ca>
2021-01-16 15:36:48 -0500

    pages init w folder

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,9 @@
 
+import { BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import './App.css';
+import HomePage from './Pages/Home.js'
+import Kanban from './Pages/Kanban.js'
+import Groups from './Pages/Groups.js'
 
 
 function App() {
@@ -8,9 +12,25 @@ function App() {
       <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3pro.css"></link>
       <header className="App-header">
-      
+        
         <div class="mobile-root" >
-          test this out
+       
+          <Router>
+
+            <ul className="app-nav-bar">
+              <li className="app-nav-bar"><Link to="/kanban">kanban</Link></li>
+              <li className="app-nav-bar"><Link to="/groups">groups</Link></li>
+              <li className="app-nav-bar"><Link to="/">home</Link></li>
+            </ul>
+
+            <Switch>
+              <Route path='/kanban' component={Kanban}/>
+              <Route path='/groups' component={Groups}/>
+              <Route path='/' component={HomePage}/>
+            </Switch>
+
+        </Router>
+
         </div>
 
       </header>
@@ -19,3 +39,10 @@ function App() {
 }
 
 export default App;
+
+/* Temporary commented out 
+      <Route path='/kanban' component={}/>
+          <Route path='/groups' component={}/>
+
+
+*/
